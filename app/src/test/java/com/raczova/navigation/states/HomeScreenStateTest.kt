60f36c116512d16da8d7b040085e0ce640cc0ccc
zuzana.raczova <zuzanaraczova99@gmail.com>
2025-01-14 14:41:53 +0100

    Revert "added unit tests"
    This reverts commit 3caa7c5a92cdf23fd35db8a5796d1d073dd36c0c.

--- app/src/test/java/com/raczova/navigation/states/HomeScreenStateTest.kt
+++ app/src/test/java/com/raczova/navigation/states/HomeScreenStateTest.kt
@@ -5,11 +5,7 @@ import android.content.SharedPreferences
 import com.raczova.navigation.R
 import com.raczova.navigation.models.watering.HouseLocation
 import com.raczova.navigation.models.watering.PlantModel
-import io.mockk.Runs
-import io.mockk.every
-import io.mockk.just
-import io.mockk.mockk
-import io.mockk.verify
+import io.mockk.*
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
