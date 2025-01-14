c5a2770eb745d172da26f3d89762f45657cd113a
zuzana.raczova <zuzanaraczova99@gmail.com>
2025-01-14 23:46:08 +0100

    test3

--- app/src/test/java/com/raczova/navigation/states/HomeScreenStateTest.kt
+++ app/src/test/java/com/raczova/navigation/states/HomeScreenStateTest.kt
@@ -48,6 +48,7 @@ class HomeScreenStateTest {
     @Test
     fun getSelectedPlant_returnsSelectedPlant() {
         val plant = PlantModel(
+            id = "",
             "Rose",
             description = "",
             location = HouseLocation.BEDROOM,
